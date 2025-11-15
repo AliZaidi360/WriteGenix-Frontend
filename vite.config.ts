e4635d934c1aeef8051e9cf0f3f4e43a50dd632b
Syed Zaidi <138819593+AliZaidi360@users.noreply.github.com>
2025-11-15 14:28:10 -0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,19 +3,13 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 
 export default defineConfig({
-  root: "client", // 👈 SUPER IMPORTANT
   plugins: [react()],
 
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "client/src"),
-      "@assets": path.resolve(__dirname, "client/public/assets"),
+      "@": path.resolve(__dirname, "src"),
+      "@assets": path.resolve(__dirname, "public/assets"),
     },
-  },
-
-  build: {
-    outDir: "../dist", // 👈 Put output in root/dist
-    emptyOutDir: true,
   },
 
   server: {
